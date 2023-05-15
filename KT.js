d618e86a25d7a942c049bd0bf81e5673c0e72bc4
impronen <impronen@gmail.com>
2023-05-15 19:53:40 +0300

    graphPlayground + early stage

--- KT.js
+++ KT.js
@@ -1,1 +1,23 @@
-class gameboard {}
+class Node {
+  constructor(boardSquare) {
+    this.square = boardSquare;
+  }
+}
+
+class knight {
+  constructor(start = null, goal = null) {
+    this.moves = [
+      [1, 2],
+      [2, 1],
+      [2, -1],
+      [1, -2],
+      [-1, -2],
+      [-2, -1],
+      [-2, 1],
+      [-1, 2],
+    ];
+    this.start = start;
+    this.goal = goal;
+    this.path = new Set();
+  }
+}
